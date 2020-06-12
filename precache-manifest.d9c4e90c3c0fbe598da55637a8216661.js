15586fe00e61530531c961fd43122e66084415fe
“Gio” <reactivegio@gmail.com>
2020-06-12 14:51:28 +0200

    Updates

---
+++ precache-manifest.d9c4e90c3c0fbe598da55637a8216661.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84ac71564c0a62545a242d2aec075a77",
+    "url": "/uploader_with_progress/index.html"
+  },
+  {
+    "revision": "d8b34e9c7a534634bae7",
+    "url": "/uploader_with_progress/static/css/main.5eb9dc9d.chunk.css"
+  },
+  {
+    "revision": "7a7cccb93fabd41352b8",
+    "url": "/uploader_with_progress/static/js/2.e0cc20aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/uploader_with_progress/static/js/2.e0cc20aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8b34e9c7a534634bae7",
+    "url": "/uploader_with_progress/static/js/main.e573741d.chunk.js"
+  },
+  {
+    "revision": "4a7125d831f5613565b7",
+    "url": "/uploader_with_progress/static/js/runtime-main.7e0c65b8.js"
+  },
+  {
+    "revision": "456b05f8c529285f0b4aba1e1a36d8f9",
+    "url": "/uploader_with_progress/static/media/checked.456b05f8.svg"
+  },
+  {
+    "revision": "bbad8f0244013e0da9c137adafe7e3d3",
+    "url": "/uploader_with_progress/static/media/close.bbad8f02.svg"
+  },
+  {
+    "revision": "84cefa90e376aed94be54d3d49c03467",
+    "url": "/uploader_with_progress/static/media/file.84cefa90.svg"
+  },
+  {
+    "revision": "c2420f0d1672e8e682fb456911c77e50",
+    "url": "/uploader_with_progress/static/media/upload.c2420f0d.svg"
+  }
+]);
--- src/utils/index.js
+++
@@ -1,9 +0,0 @@
-export function formatBytes(bytes, decimals) {
-  if (bytes === 0) return "0 Bytes";
-  let k = 1000;
-  let dm = decimals + 1 || 3;
-  let sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-  let i = Math.floor(Math.log(bytes) / Math.log(k));
-  //return numeral(parseFloat((bytes / Math.pow(k, i)).toFixed(dm))).format("0.0,") + " " + sizes[i];
-  return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)) + " " + sizes[i];
-}
